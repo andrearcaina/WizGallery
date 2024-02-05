cbf82ff994a73085ee57d64eba13eb1489465a94
Andre <dtandre331@gmail.com>
2024-02-05 10:12:05 -0500

    project structure

--- frontend/src/components/index.jsx
+++ frontend/src/components/index.jsx
@@ -1,6 +1,6 @@
 'use client';
-export { default as Form } from './form';
-export { default as Search } from './search';
-export { default as Navbar } from './navbar/nav';
-export { default as Footer } from './footer';
-export { default as ScrollUp } from './scroll-up';
+export { default as Form } from './general/form';
+export { default as Search } from './general/search';
+export { default as Navbar } from './general/navbar/nav';
+export { default as Footer } from './general/footer';
+export { default as ScrollUp } from './general/scroll-up';
